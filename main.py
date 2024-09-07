eda82952515cdbe7b2060666999d834c9def216e
Agustina <agustinagryner@gmail.com>
2024-09-07 13:53:39 -0300

    Commit

--- main.py
+++ main.py
@@ -1,2 +1,50 @@
-# Test
-print("Test")
+# JUEGO DE ROL: EL LEGADO PERDIDO
+
+import random
+
+def verificar_nombre(nombre):
+    while not (nombre.isalpha()):
+        print("El nombre debe estar únicamente compuesto por letras.")
+        nombre = input("Intenta nuevamente: ")
+    nombre = nombre.upper()
+    
+    return nombre
+
+def ataque():
+    daño = random.randint(40, 80)
+    return daño
+
+def turno_tanque(tanque): #REVISAR!
+    print(f"TURNO DE {tanque}")
+    print("Atacar - Bloquear - Items")
+    accion = input(f"Qué hará {tanque}?:").capitalize()
+    while (accion != "Atacar") or (accion != "Bloquear") or (accion != "Items"):
+        print("Acción inválida.")
+        accion = input(f"Qué hará {tanque}?:").capitalize()
+    
+def main():
+    print("Nombra a tus personajes. Solo se permiten caracteres alfabeticos.")
+    n = input("TANQUE: ")
+    tanque = verificar_nombre(n)
+    n = input("BRUJO: ")
+    brujo = verificar_nombre(n)
+    n = input("ARQUERO: ")
+    arquero = verificar_nombre(n)
+    
+    print(f"Tus personajes son {tanque}, {brujo} y {arquero}")
+    
+    TANQUE_HP = 200
+    BRUJO_HP = 150
+    ARQUERO_HP = 100
+    ENEMIGO_HP = 1000
+    
+    print("Se acerca un enemigo")
+    
+    while ENEMIGO_HP > 0:
+        daño = turno_tanque(tanque)
+        ENEMIGO_HP -= daño
+        print(daño)
+        print(f"ENEMIGO: {ENEMIGO_HP})")
+        
+if __name__=="__main__":
+    main()
