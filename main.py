2852c59280e5483f80d16ada04c237d9a41a0293
ariana progano <elmallian@gmail.com>
2024-09-09 16:29:15 -0300

    cad de caracteres

--- main.py
+++ main.py
@@ -140,9 +140,13 @@ def main():
         jugador_index = (jugador_index + 1) % len(jugadores)  # Reinicia el turno cuando llega al último jugador
     
     if enemigo_index == len(enemigos) and sum(jugadores_hp) > 0:
-        print("¡Has derrotado a todos los enemigos!")
+        cad1="¡HAS DERROTADO A TODOS LOS ENEMIGOS!"
+        cad2=cad1.center(50,"=")
+        print(cad2)
     else:
-        print("¡Has sido derrotado por los enemigos!")
+        cad1="¡HAS DERROTADO A TODOS LOS ENEMIGOS!"
+        cad2=cad1.center(50,"=")
+        print(cad2)
         
 if __name__ == "__main__":
     main()
