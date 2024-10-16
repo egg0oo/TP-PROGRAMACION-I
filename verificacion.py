1a666577b29de6ddd99b7fa4ddadb80215954e90
Agustina <agustinagryner@gmail.com>
2024-10-16 11:18:03 -0300

    Mejora de módulo de verificación

--- verificacion.py
+++ verificacion.py
@@ -1,16 +1,14 @@
-def verificarnombre():
-    tan = input("TANQUE: ")
-    while not (tan.isalpha()):
-        print("El nombre del TANQUE debe estar únicamente compuesto por letras.")
-        tan = input("Intenta nuevamente: ")
-    bru = input("BRUJO: ")
-    while not (bru.isalpha()):
-        print("El nombre del BRUJO debe estar únicamente compuesto por letras.")
-        bru = input("Intenta nuevamente: ")
-    ar = input("ARQUERO: ")
-    while not (ar.isalpha()):
-        print("El nombre del ARQUERO debe estar únicamente compuesto por letras.")
-        ar = input("Intenta nuevamente: ")
-    Mayusculas = lambda a, b, c: (a.upper(), b.upper(), c.upper())
-    tan, bru, ar = Mayusculas(tan, bru, ar)
-    return tan, bru, ar
+def nombrar(personaje):
+    nombre = input("Ingresa el nombre: ")
+    if verificar(nombre):
+        return nombre
+    else:
+        print("El nombre es inválido. Intente nuevamente.")
+        nombre = nombrar(personaje)
+        return nombre
+
+def verificar(nombre):
+    if nombre.isalpha() == True:
+        return True
+    else:
+        return False
