f3add1598b136a3e82f2625ae3d719c40db2383e
Agustina <agustinagryner@gmail.com>
2024-10-23 10:21:48 -0300

    Nombre de personajes en diccionario

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from verificacion import nombrar
 from verificacion import verificar
 
-def cargar_stats(archivo):
+def cargar_stats(archivo, nombres):
     try:
         personajes = {}
         file = open(archivo, "r")
@@ -10,18 +10,19 @@ def cargar_stats(archivo):
     else:
         for linea in file:
             datos = linea.strip().split(";")
-            personaje = datos[0]
+            rol = datos[0]
             vida = int(datos[1])
             acciones = datos[2:]
-            personajes[personaje] = {
-                "vida":vida,
-                "acciones":acciones,
-                "daño total":[0,0,0]
-                }
-        linea = file.readline()
+
+            # Para reemplazar por su nombre asignado.
+            personajes[nombres[rol]] = {
+                 "vida": vida,
+                 "acciones": acciones,
+                 "daño total": [0, 0, 0]
+            }
         file.close()
         return personajes
-    
+
 def cargar_stats_enemigos(archivo):
     try:
         enemigos = {}
@@ -35,10 +36,9 @@ def cargar_stats_enemigos(archivo):
             vida = int(datos[1])
             ataque = int(datos[2])
             enemigos[enemigo] = {
-                "vida":vida,
-                "ataque":ataque,
-                }
-        linea = file.readline()
+                "vida": vida,
+                "ataque": ataque,
+            }
         file.close()
         return enemigos
 
@@ -46,28 +46,28 @@ def main():
     cad = "EL LEGADO PERDIDO".center(29, "=")
     print(cad)
     
-    print("En las sombras de un imperio muerto, susurran los ecos de un poder olvidado.")
-    print("Aeldenor, se encuentra en ruinas, consumido por una maldición.")
-    print("Los descendientes de los corrompidos, herederos de una tragedia ancestral, buscan respuestas en un mundo que ha olvidado su propia condena.")
-    print("Al enfrentarse a los enemigos irás descubriendo más de la historia de Aeldenor y el secreto que conlleva la maldición.")
-    print()
-    
     print("Nombra a tus personajes. Solo se permiten caracteres alfabéticos.")
     
     tanque = nombrar("TANQUE")
     print(f"El nombre de tu TANQUE es {tanque}")
+    
     brujo = nombrar("BRUJO")
     print(f"El nombre de tu BRUJO es {brujo}")
+    
     arquero = nombrar("ARQUERO")
     print(f"El nombre de tu ARQUERO es {arquero}")
-    
-    personajes = cargar_stats("stats.text")
-    
+
+    nombres = {
+        "TANQUE": tanque,
+        "BRUJO": brujo,
+        "ARQUERO": arquero
+    }
+
+    personajes = cargar_stats("stats.text", nombres)
     print(personajes)
     
     enemigos = cargar_stats_enemigos("stats_enemigos.text")
-    
     print(enemigos)
-    
+
 if __name__ == "__main__":
     main()
