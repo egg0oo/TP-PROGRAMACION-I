5d9d7386e6ce3ee88e4cc7ed04d083b690734174
ariana progano <elmallian@gmail.com>
2024-10-29 23:19:55 -0300

    mostrar estado

--- estado.py
+++ estado.py
@@ -1,1 +1,8 @@
-# -
+def mostrar_estado(personajes, enemigos, enemigo_actual):
+    print()
+    print("===== ESTADO ACTUAL =====")    
+    for nombre in personajes:
+        print(f"{nombre}: {personajes[nombre]['vida']} HP")
+    print(f"{enemigo_actual}: {enemigos[enemigo_actual]['vida']} HP")
+    print("==========================")
+    print()
